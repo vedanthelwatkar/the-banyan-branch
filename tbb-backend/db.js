a18b983199bb34de3b5c30b25bfe7c99fb9d36f6
helwatkarVedant <vedant@unifynd.com>
2025-01-11 19:42:26 +0530

    env added in db

--- tbb-backend/db.js
+++ tbb-backend/db.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2";
 
 export const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "Vedantph@2201",
-  database: "the_banyan_branch",
-  port: "3306",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 connection.connect((err) => {
